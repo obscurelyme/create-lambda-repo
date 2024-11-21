9288c7c559f5fc85c7e629b3abf59b1bf6c3354a
obscurelyme <nico.frozencoffee@gmail.com>
2024-11-20 23:19:34 -0800

    function complete

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Octokit } from "octokit"
+import { env } from 'node:process'
+
+interface Payload {
+  templateRepo: string;
+  templateOwner: string;
+  owner: string;
+  repositoryName: string;
+  repositoryDescription: string;
+  visibility: 'public' | 'private' | 'internal';
+}
+
+// TODO: map the payload template to custom templates provided here...
+
+export async function handler(event: Payload) {
+  const client = new Octokit({
+    auth: env["GH_TOKEN"]
+  });
+
+  const response = await client.request(`POST /repos/${event.templateOwner}}/${event.templateRepo}/generate`, {
+    template_owner: event.templateOwner,
+    template_repo: event.templateRepo,
+    owner: event.owner,
+    name: event.repositoryName,
+    description: event.repositoryDescription,
+    include_all_branches: false,
+    private: false,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  console.log(event)
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify(response.data)
+  }
+}
